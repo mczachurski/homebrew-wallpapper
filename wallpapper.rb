c8fa9f6635e69aaec1d5e20f8b1b5bc207b5c963
Marcin Czachurski <mczachurski@icloud.com>
2018-07-12 11:09:57 +0200

    Update sha.

--- wallpapper.rb
+++ wallpapper.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Wallpapper < Formula
   homepage 'https://github.com/mczachurski/wallpapper'
   url 'https://github.com/mczachurski/wallpapper/tarball/1.0.0'
-  sha256 'fb0032448d83087534be489669096632a43b00050f640f041160905ff1c5f170'
+  sha256 '5197790cf6a50cce2a3f9d72a7b123edc3b56c8aa43696dedd7d5b4cb96fa461'
 
   head 'https://github.com/mczachurski/wallpapper.git'
 
