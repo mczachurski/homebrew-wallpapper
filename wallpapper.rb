0ef076f05fb14f462c8d3087875a791078c6e655
Marcin Czachurski <mczachurski@icloud.com>
2018-08-03 08:24:44 +0200

    Change url to package.

--- wallpapper.rb
+++ wallpapper.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Wallpapper < Formula
   homepage 'https://github.com/mczachurski/wallpapper'
-  url 'https://github.com/mczachurski/wallpapper/tarball/1.1.0'
+  url 'https://github.com/mczachurski/wallpapper/archive/1.1.0.tar.gz'
   sha256 '931cec74d4e5537d6a24c3fd1a6e1cc8ca045a05feba9e5b1a36f49440792370'
 
   head 'https://github.com/mczachurski/wallpapper.git'
