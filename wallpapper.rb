1cd81be2e9a3fb0650fbd846156f3d790a39d458
Marcin Czachursk <mczachurski@icloud.com>
2022-05-22 11:22:14 +0200

    Change to 1.7.3 version

--- wallpapper.rb
+++ wallpapper.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Wallpapper < Formula
   homepage 'https://github.com/mczachurski/wallpapper'
-  url 'https://github.com/mczachurski/wallpapper/archive/1.7.2.tar.gz'
-  sha256 '9f8dc5059256731903daa66e510332b6b4a5e3f217ba658c208acbc3d5d97dd6'
+  url 'https://github.com/mczachurski/wallpapper/archive/1.7.3.tar.gz'
+  sha256 '517965884f29adfa76aa11bc1f7589eb53605c695f8e7c799545861dc13e5d27'
 
   head 'https://github.com/mczachurski/wallpapper.git'
 
